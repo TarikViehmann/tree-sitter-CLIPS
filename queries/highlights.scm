b93a1088defe1577771952bb2fafe68ae33f3ebd
Tarik Viehmann <viehmann@kbsg.rwth-aachen.de>
2025-12-03 14:03:14 +0100

    initial commit

---
+++ queries/highlights.scm
@@ -0,0 +1,12 @@
+; highlights.scm
+
+"defrule" @keyword
+
+(variable) @variable.builtin
+(name) @type.builtin
+(function_name) @function
+(comment_line) @comment
+(comment) @string.special
+(number) @number
+(query) @constructor
+(string) @string
---
+++ queries/indents.scm
@@ -0,0 +1,15 @@
+[
+ (construct)
+ (query)
+ (then)
+ (else)
+]@indent.begin
+
+((template_RHS_pattern) @indent.align
+ (#set! indent.open_delimiter "(")
+ (#set! indent.close_delimiter ")")
+)
+((if_statement) @indent.align
+ (#set! indent.open_delimiter "(")
+ (#set! indent.close_delimiter ")")
+)
